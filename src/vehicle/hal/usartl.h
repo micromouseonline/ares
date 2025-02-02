56cee19ba6f353cd86cabf836144b97bb9fbf260
Peter Harrison <peter.harrison@helicron.net>
2025-02-02 15:53:17 +0000

    add stub for USART device

---
+++ src/vehicle/hal/usartl.h
@@ -0,0 +1,30 @@
+//
+// Created by peter on 02/02/25.
+//
+
+#pragma once
+
+/***
+ * Stub for the vehicle's primary serial device
+ *
+ * this is n IO device so we need to implement inputs as well
+ *
+ * On MR32 this inherits from Print
+ */
+
+class Usart {
+ public:
+  void write(const char *str) {
+    (void)str;
+  }
+
+  int println(const char *str) {
+    (void)str;
+    return 0;
+  }
+
+  int puts(const char *str) {
+    (void)str;
+    return 0;
+  }
+};
