ef8f203802f9a05656a325c09c41e424a6024b5e
Peter Harrison <peter.harrison@helicron.net>
2025-02-02 15:43:30 +0000

    make Vehicle a singleton and make delay_ms() a free function

--- src/vehicle/hal/speaker.h
+++ src/vehicle/hal/speaker.h
@@ -5,6 +5,69 @@
 
 #include "common/delay.h"
 
-inline void speaker() {
-  delay_ms(33);
-}
+/**
+ * A speaker interface class
+ *
+ * There are no real speakers in the simulator and there seem to be a lot
+ * of specific methods. It might be best to reduce these to the bare minimum
+ *
+ * Meanwhile it might be a good idea if the issue a message or log entry to
+ * show they have been called. The simulator could actually d i the sounds I guess
+ */
+
+class Speaker {
+ public:
+  void begin() {
+    /// sets up the hardware
+  }
+
+  void on() {
+    /// connects the timer to the output pin
+  }
+
+  void off() {
+    /// diconnects the timer from the output pin
+  }
+
+  /// called from systick to update the speaker
+  void update() {
+  }
+
+  void playTone(uint32_t frequency, uint32_t duration) {
+    delay_ms(duration);
+  }
+
+  void confirm() {
+  }
+
+  void k() {
+  }
+
+  void exclaim() {
+  }
+
+  void good() {
+  }
+
+  void alert() {
+  }
+
+  void cricket(uint32_t pattern) {
+    (void)pattern;
+  }
+
+  void trill() {
+  }
+
+  void complete() {
+  }
+
+  void success() {
+  }
+
+  void fail() {
+  }
+
+  void error() {
+  }
+};
