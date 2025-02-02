d72f8292a43493149ae4687374a53223b445d6bf
Peter Harrison <peter.harrison@helicron.net>
2025-02-02 15:48:58 +0000

    add stub for vehicle display

---
+++ src/vehicle/hal/display.h
@@ -0,0 +1,22 @@
+//
+// Created by peter on 02/02/25.
+//
+
+#pragma once
+
+/**
+ * Stub for an on-board display
+ */
+class Display {
+ public:
+  void printf(const char *format, ...) {
+
+  };
+
+  int puts(char *str) {
+    return 0;
+  };
+
+  void cls() {
+  }
+};
