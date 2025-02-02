aaaa5f34febd450eb9b1d274b36fd010f11134fc
Peter Harrison <peter.harrison@helicron.net>
2025-02-02 15:55:15 +0000

    add stub for button

---
+++ src/vehicle/hal/button.h
@@ -0,0 +1,24 @@
+//
+// Created by peter on 02/02/25.
+//
+
+#pragma once
+
+/***
+ * Stub for a single button
+ *
+ */
+
+class Button {
+ public:
+  bool isPressed() {
+    return false;
+  }
+
+  void waitForRelease() {
+  }
+
+  int getPressType() {
+    return 0;
+  }
+};
